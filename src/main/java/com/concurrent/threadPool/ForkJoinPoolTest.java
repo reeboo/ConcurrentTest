b52a6f22849eab2966e82cf44ffdd13b83cbee79
reeboo <ruibo.ma@qunar.com>
2014-08-24 10:52:54 +0800

    forkjoin test

--- src/main/java/com/concurrent/threadPool/ForkJoinPoolTest.java
+++ src/main/java/com/concurrent/threadPool/ForkJoinPoolTest.java
@@ -1,5 +1,12 @@
 package com.concurrent.threadPool;
 
+import java.io.File;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 /**
  * 实现描述: ForkJoinPoolTest
  *
@@ -7,5 +14,36 @@ package com.concurrent.threadPool;
  * @author: reeboo
  * @since: 2014-08-24 09:58
  */
-public class ForkJoinPoolTest {
+public class ForkJoinPoolTest extends RecursiveTask<Integer> {
+    private Path dir;
+
+    public ForkJoinPoolTest(Path dir) {
+        this.dir = dir;
+    }
+
+    public static void main(String args[]) {
+        Integer count = new ForkJoinPool().invoke(new ForkJoinPoolTest(Paths.get("/usr")));
+        System.out.println(count);
+    }
+
+    @Override
+    protected Integer compute() {
+        int count = 0;
+        try {
+            DirectoryStream<Path> ds = Files.newDirectoryStream(dir);
+            for (Path subPath : ds) {
+                if (Files.isDirectory(subPath, LinkOption.NOFOLLOW_LINKS)) {
+                    ForkJoinPoolTest subTask = new ForkJoinPoolTest(subPath);
+                    subTask.fork();
+                    count += subTask.join();
+                } else {
+                    count++;
+                }
+            }
+        } catch (Exception ex) {
+            return 0;
+        }
+        return count;
+    }
 }
+

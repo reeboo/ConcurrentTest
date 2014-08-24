74818b4cc24b98a5b879c60ec53db400579347aa
reeboo <ruibo.ma@qunar.com>
2014-08-24 10:45:55 +0800

    forkjoin 测试

--- src/main/java/com/concurrent/cyclicberry/CyclicBarrierTest.java
+++ src/main/java/com/concurrent/cyclicberry/CyclicBarrierTest.java
@@ -6,13 +6,20 @@ import java.util.Random;
 import java.util.concurrent.*;
 
 /**
- * 实现描述: CyclicBarrierDemo
- *
+ * CyclicBarrier类似于CountDownLatch也是个计数器，
+ * 不同的是CyclicBarrier数的是调用了CyclicBarrier.await()进入等待的线程数，
+ * 当线程数达到了CyclicBarrier初始时规定的数目时，所有进入等待状态的线程被唤醒并继续。
+ * CyclicBarrier就象它名字的意思一样，可看成是个障碍，
+ * 所有的线程必须到齐后才能一起通过这个障碍。
+ * CyclicBarrier初始时还可带一个Runnable的参数，
+ * 此Runnable任务在CyclicBarrier的数目达到后，所有其它线程被唤醒前被执行。
+ * <p/>
  * @version v1.0.0
+ *
  * @author: reeboo
  * @since: 2014-08-23 18:23
  */
-public class CyclicBarrierDemo {
+public class CyclicBarrierTest {
 
     @Test
     public void testAll() throws Exception {
---
+++ src/main/java/com/concurrent/threadPool/ForkJoinPoolTest.java
@@ -0,0 +1,11 @@
+package com.concurrent.threadPool;
+
+/**
+ * 实现描述: ForkJoinPoolTest
+ *
+ * @version v1.0.0
+ * @author: reeboo
+ * @since: 2014-08-24 09:58
+ */
+public class ForkJoinPoolTest {
+}

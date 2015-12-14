9b8a74b35e20639cddd42a42180ba80f66891c5a
reeboo <ruibo.ma@qunar.com>
2015-12-14 11:00:36 +0800

    完善reentrantlock

---
+++ src/main/java/com/concurrent/juc/aqs/AQScustomDeFine.java
@@ -0,0 +1,73 @@
+package com.concurrent.juc.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 实现描述: AQScustomDeFine
+ *
+ * @version v1.0.0
+ * @author: reeboo
+ * @since: 2015-12-14 10:54
+ */
+public class AQScustomDeFine implements Lock {
+
+    private class Sync extends AbstractQueuedSynchronizer{
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return super.tryAcquire(arg);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            return super.tryRelease(arg);
+        }
+
+        @Override
+        protected int tryAcquireShared(int arg) {
+            return super.tryAcquireShared(arg);
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            return super.tryReleaseShared(arg);
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return super.isHeldExclusively();
+        }
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
